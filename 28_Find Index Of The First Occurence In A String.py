1b6b1df5b73ab85c883fd71719c7e518ae7685fa
alek <alek.schmierer2022@gmail.com>
2025-07-11 16:03:25 -0700

    Find Index Of The First Occurence In A String

---
+++ 28_Find Index Of The First Occurence In A String.py
@@ -0,0 +1,10 @@
+#Understand
+#Return index of the first occurnece of needle in haystack, or -1 if not
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
+
+#Time Complexity: O(n)
+#Space Complexity: O(1)
+#Completed 7/11/25
+#Time: 0:34
